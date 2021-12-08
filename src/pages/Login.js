b71461eb2447715e3d735b7b2bf7d2ac3ce4c523
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-08 16:25:15 -0300

    Adiciona lógica de valida do  botão de enviar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const getDisabled = (email, password) => {
+  const checkEmail = () => {
+    const verify = /\S+@\S+\.\S+/;
+    return verify.test(email);
+  };
+  const passwordCharacters = 6;
+  const checkPassword = () => password.length > passwordCharacters;
+
+  return checkEmail() && checkPassword();
+};
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <form>
+    <form className="container form-group">
       <label htmlFor="email">
         <input
           id="email"
           type="email"
           data-testid="email-input"
+          value={ email }
+          onChange={ ({ target }) => setEmail(target.value) }
         />
       </label>
       <label htmlFor="password">
-        <input id="password" type="password" data-testid="password-input" />
+        <input
+          id="password"
+          type="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
       </label>
-      <button type="submit" data-testid="login-submit-btn">Entrar</button>
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+        disabled={ !getDisabled(email, password) }
+      >
+        Entrar
+
+      </button>
     </form>
   );
 }
