78b353535d112b434d4347ce281c9f662bb03aa0
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-09 17:04:42 -0300

    Adiciona base da página de receitas feitas

--- src/pages/RecipesMade.js
+++ src/pages/RecipesMade.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import CardRecipesMade from '../components/CardRecipesMade';
 
 function RecipesMade() {
+  // pode ser modificado
   return (
     <div>
       <h1>Receitas feitas</h1>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+      >
+        Drinks
+      </button>
+      <CardRecipesMade />
     </div>
   );
 }
