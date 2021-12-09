81e6389d85a3620e063f6cd2d7eaf28f768068f3
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-12-09 06:23:45 -0300

    Implementando requisiçoes as API's - Provisao de dados ao contexto

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,15 +1,43 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import fetchMealsCategories from '../services/fetchMealsCategories';
+import fetchAreas from '../services/fetchAreas';
+import fetchIngredients from '../services/fetchIngredients';
+import fetchMeals from '../services/fetchMeals';
+import fetchDrinks from '../services/fetchDrinks';
 
 const Context = createContext();
 const { Provider, Consumer } = Context;
 
 function RecipesProvider({ children }) {
   const [email, setEmail] = useState('');
+  const [meals, setMeals] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+  const [mealsCategories, setMealsCategories] = useState([]);
+  const [areas, setAreas] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetchMeals().then((response) => setMeals(response.meals));
+    fetchDrinks().then((response) => setDrinks(response.drinks));
+    fetchMealsCategories().then((response) => setMealsCategories(response.meals));
+    fetchAreas().then((response) => setAreas(response.meals));
+    fetchIngredients().then((response) => setIngredients(response.meals));
+  }, []);
 
   const context = {
     email,
+    meals,
+    drinks,
+    mealsCategories,
+    areas,
+    ingredients,
     setEmail,
+    setMeals,
+    setDrinks,
+    setMealsCategories,
+    setAreas,
+    setIngredients,
   };
 
   return (
