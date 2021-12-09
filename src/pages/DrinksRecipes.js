e03199adc7965a883249af06582271339b4e5b57
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-09 19:58:08 -0300

    Renderiza componente de Card de receitas de somente bebidas baseado na condição

---
+++ src/pages/DrinksRecipes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecipeCard from '../components/RecipeCard';
+
+function DrinksRecipes() {
+  return (
+    <div>
+      <h1>Receitas de comidas</h1>
+      <RecipeCard itemToMap="drinks" />
+    </div>
+  );
+}
+
+export default DrinksRecipes;
