0f972620948e7cd8277d5e3f7ed5108e97e7f1c9
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-10 19:33:27 -0300

    Adiciona botãos dos filtros de 5 categorias de bebidas

--- src/pages/DrinksRecipes.js
+++ src/pages/DrinksRecipes.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import RecipeCard from '../components/RecipeCard';
+import { Context } from '../context/Context';
 
 function DrinksRecipes() {
+  const { drinksCategories } = useContext(Context);
+  const maxCategories = 5;
   return (
     <div>
       <h1>Receitas de comidas</h1>
+      {drinksCategories.map((categorieDrink, index) => {
+        if (index < maxCategories) {
+          return (
+            <button
+              data-testid={ `${categorieDrink.strCategory}-category-filter` }
+              type="button"
+              key={ categorieDrink.strCategory }
+            >
+              {categorieDrink.strCategory}
+            </button>
+          );
+        }
+        return null;
+      })}
       <RecipeCard itemToMap="drinks" />
     </div>
   );
