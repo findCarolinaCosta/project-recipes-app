49a541ad061c6552a713df1c607872ecd0720d30
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-10 19:33:05 -0300

    Adiciona botãos dos filtros de 5 categorias de comidas

--- src/pages/FoodsRecipes.js
+++ src/pages/FoodsRecipes.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import RecipeCard from '../components/RecipeCard';
+import { Context } from '../context/Context';
 
 function FoodsRecipes() {
+  const { mealsCategories } = useContext(Context);
+  const maxCategories = 5;
   return (
     <div>
       <h1>Receitas de comidas</h1>
+      { mealsCategories.map((categorieMeal, index) => {
+        if (index < maxCategories) {
+          return (
+            <button
+              data-testid={ `${categorieMeal.strCategory}-category-filter` }
+              type="button"
+              key={ categorieMeal.strCategory }
+            >
+              {categorieMeal.strCategory}
+
+            </button>
+          );
+        }
+        return null;
+      })}
       <RecipeCard itemToMap="meals" />
     </div>
   );
