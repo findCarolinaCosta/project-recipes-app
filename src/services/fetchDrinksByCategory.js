f6e9c7c2a0c66bda1edcd8965dc45135f6f68010
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-01-03 16:55:36 -0300

    Altera buscas para retornar promises ja com os itens em uma lista e nao a lista dentro de um objeto

--- src/services/fetchDrinksByCategory.js
+++ src/services/fetchDrinksByCategory.js
@@ -2,7 +2,7 @@ const fetchDrinksByCategory = async (receivedCategory) => {
   const API_URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${receivedCategory}`;
   const response = await fetch(API_URL);
   const jsonResponse = await response.json();
-  return jsonResponse;
+  return jsonResponse.drinks;
 };
 
 export default fetchDrinksByCategory;
--- src/services/fetchDrinksCategories.js
+++ src/services/fetchDrinksCategories.js
@@ -2,7 +2,7 @@ const fetchDrinksCategories = async () => {
   const API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
   const response = await fetch(API_URL);
   const jsonResponse = await response.json();
-  return jsonResponse;
+  return jsonResponse.drinks;
 };
 
 export default fetchDrinksCategories;
