4222c70e13f62508d246e9de9620435d05af25ad
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-01-03 16:38:53 -0300

    Adiciona funçao de busca de bebidas por categoria

---
+++ src/services/fetchDrinksByCategory.js
@@ -0,0 +1,8 @@
+const fetchDrinksByCategory = async (receivedCategory) => {
+  const API_URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${receivedCategory}`;
+  const response = await fetch(API_URL);
+  const jsonResponse = await response.json();
+  return jsonResponse;
+};
+
+export default fetchDrinksByCategory;
