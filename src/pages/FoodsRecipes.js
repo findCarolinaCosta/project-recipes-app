df3a545a797d5a36bdee6aec219b010ce5013dc2
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-01-04 19:18:23 -0300

    Adiciona lógica do filtrar ou não ao clicar no botões de comidas

--- src/pages/FoodsRecipes.js
+++ src/pages/FoodsRecipes.js
@@ -1,20 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import RecipeCard from '../components/RecipeCard';
 import { Context } from '../context/Context';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import fetchMealsByCategories from '../services/fetchMealsByCategories';
+import fetchMeals from '../services/fetchMeals';
 
 function FoodsRecipes(props) {
   const { history } = props;
   const { mealsCategories, setMeals } = useContext(Context);
   const maxCategories = 5;
+  const [nameBtn, setnameBtn] = useState('');
 
-  const filteredByCategory = (categoryName) => {
-    fetchMealsByCategories(categoryName)
-      .then((response) => setMeals(response));
+  const filteredByCategory = (categoryName, event) => {
+    if (nameBtn === event.target.name) {
+      fetchMeals().then((response) => setMeals(response.meals));
+      setnameBtn('');
+    } else {
+      fetchMealsByCategories(categoryName)
+        .then((response) => setMeals(response));
+      setnameBtn(event.target.name);
+    }
   };
+
   return (
     <div className="recipes-container container-fluid">
       <header className="row">
@@ -27,7 +36,8 @@ function FoodsRecipes(props) {
               data-testid={ `${categorieMeal.strCategory}-category-filter` }
               type="button"
               key={ categorieMeal.strCategory }
-              onClick={ () => filteredByCategory(categorieMeal.strCategory) }
+              name={ categorieMeal.strCategory }
+              onClick={ (event) => filteredByCategory(categorieMeal.strCategory, event) }
             >
               {categorieMeal.strCategory}
 
