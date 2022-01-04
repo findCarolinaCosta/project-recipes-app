eeeeafe65e901c5d4bd2866036996c84d2d4a0fc
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-01-04 19:43:46 -0300

    Adiciona botão de filtro de todas as categorias de bebidas

--- src/pages/DrinksRecipes.js
+++ src/pages/DrinksRecipes.js
@@ -28,22 +28,34 @@ function DrinksRecipes(props) {
       <header className="row">
         <Header className="container-fluid" props={ props } />
       </header>
-      {drinksCategories.map((categorieDrink, index) => {
+      {drinksCategories.map((categoriseDrink, index) => {
         if (index < maxCategories) {
           return (
             <button
-              data-testid={ `${categorieDrink.strCategory}-category-filter` }
+              data-testid={ `${categoriseDrink.strCategory}-category-filter` }
               type="button"
-              key={ categorieDrink.strCategory }
-              name={ categorieDrink.strCategory }
-              onClick={ (event) => filteredByCategory(categorieDrink.strCategory, event) }
+              key={ categoriseDrink.strCategory }
+              name={ categoriseDrink.strCategory }
+              onClick={
+                (event) => filteredByCategory(categoriseDrink.strCategory, event)
+              }
             >
-              {categorieDrink.strCategory}
+              {categoriseDrink.strCategory}
             </button>
           );
         }
         return null;
       })}
+      <button
+        type="button"
+        data-testid="All-category-filter"
+        onClick={
+          () => fetchDrinks()
+            .then((response) => setDrinks(response.drinks))
+        }
+      >
+        All
+      </button>
       <div className="row">
         <RecipeCard
           className="container-fluid"
