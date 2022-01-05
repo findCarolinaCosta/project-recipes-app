7de4fc92099b686366a6440101b01e9e5784b00f
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-01-05 18:57:05 -0300

    Implementando tela de Detalhes da Receita

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,16 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { Context } from '../context/Context';
+import fetchMealRecipeDetailsById from '../services/fetchMealRecipeDetailsById';
+import fetchDrinkRecipeDetailsById from '../services/fetchDrinkRecipeDetailsById';
 
 function RecipeDetails(props) {
-  console.log(props);
-  const { meals, drinks } = useContext(Context);
-  console.log(meals);
-  console.log(drinks);
+  const { setSharedProps } = useContext(Context);
+  const { match: { params: { id } }, location: { pathname } } = props;
+  const [recipeId, setRecipeId] = useState('');
+
+  useEffect(() => {
+    if (pathname.includes('comidas')) {
+      fetchMealRecipeDetailsById(id).then((response) => setRecipeId(response));
+    }
+    if (pathname.includes('bebidas')) {
+      fetchDrinkRecipeDetailsById(id).then((response) => setRecipeId(response));
+    }
+    setSharedProps(props);
+  }, [props, setSharedProps, id, pathname]);
+
   return (
-    <div className="container-sm">
-      <h1>Construindo detalhes da receita</h1>
+    <div
+      className="container recipes-container"
+      style={ { height: '100vh' } }
+    >
+      <div className="row">
+        <div className="col-sm-12">
+          <img />
+        </div>
+      </div>
     </div>
   );
 }
+
+RecipeDetails.propTypes = {
+  match: PropTypes.shape().isRequired,
+  location: PropTypes.shape().isRequired,
+};
 
 export default RecipeDetails;
