0aa4a1120c7a2c246528e1f2faf7dc49a43f8462
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-01-12 19:19:33 -0300

    Updates

---
+++ precache-manifest.46793e56706c484ffadd4c9c638fc0bc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652e3530b1f4e33e28eda410d42fdce1",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/index.html"
+  },
+  {
+    "revision": "fe97b8d31b7a0300ef0e",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "c0365c422768d5a704e6",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/css/main.a02942bd.chunk.css"
+  },
+  {
+    "revision": "fe97b8d31b7a0300ef0e",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/js/2.7ed29211.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/js/2.7ed29211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0365c422768d5a704e6",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/js/main.796f5964.chunk.js"
+  },
+  {
+    "revision": "0da6c833361613e3d559",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/js/runtime-main.1a792c95.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/findCarolinaCosta.github.io/project-recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
