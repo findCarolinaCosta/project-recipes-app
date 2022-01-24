75bda7032086a102537dbf0ba3b9ab19662b8521
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-01-23 21:28:18 -0300

    style: Add stylization of the foods and drinks explore screens

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -8,45 +8,55 @@ function ExploreDrinks() {
   const [randomRecipeID, setRandomRecipeID] = useState({});
   const fetchRandom = async () => {
     const recipeObj = await fetchRandomDrinks();
-    setRandomRecipeID(recipeObj.drinks[0].idDrink);
+    setRandomRecipeID(recipeObj.idDrink);
   };
   useEffect(() => {
     fetchRandom();
   }, []);
 
   return (
-    <div className="row">
-      <div className="col-sm-2">
+    <div className="bg-red-700 w-screen">
+      <div className=" p-3 bg-red-700 m-0">
         <Link to="/perfil">
           <button type="button">
             <img
               src={ profileIcon }
               alt="profile-icon"
-              className="header-profile-icon"
+              className="bg-red-700"
               data-testid="profile-top-btn"
             />
           </button>
         </Link>
-        <h3 className="text-center" data-testid="page-title">Explorar Bebidas</h3>
       </div>
-      <Link to="/explorar/bebidas/ingredientes">
-        <button
-          type="button"
-          className="btn btn-outline-dark"
-          data-testid="explore-by-ingredient"
+      <div className="w-screen h-screen flex flex-col">
+        <h3
+          className="text-center
+      text-red-900 font-black  p-3"
+          data-testid="page-title"
         >
-          Por Ingredientes
-        </button>
-      </Link>
-      <Link to={ `/bebidas/${randomRecipeID}` }>
-        <button
-          type="button"
-          className="btn btn-outline-dark"
-          data-testid="explore-surprise"
-        >
-          Me Surpreenda!
-        </button>
-      </Link>
+          Explorar Bebidas
+        </h3>
+        <section className="btn-group gap-4 p-3 justify-center">
+          <Link to="/explorar/bebidas/ingredientes">
+            <button
+              type="button"
+              className="btn btn-outline-danger"
+              data-testid="explore-by-ingredient"
+            >
+              Por Ingredientes
+            </button>
+          </Link>
+          <Link to={ `/bebidas/${randomRecipeID}` }>
+            <button
+              type="button"
+              className="btn btn-outline-danger"
+              data-testid="explore-surprise"
+            >
+              Me Surpreenda!
+            </button>
+          </Link>
+        </section>
+      </div>
       <Footer />
     </div>
   );
