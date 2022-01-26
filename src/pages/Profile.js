9734d00ac652103d8b64a27534f7c437b0165d8e
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-01-26 04:19:02 -0300

    style(feat): Adds profile page stylization

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -14,45 +14,62 @@ function Profile(props) {
   useEffect(() => setSharedProps(props), [props, setSharedProps]);
 
   return (
-    <div className="row">
-      <div className="col-sm-2">
+    <div className="bg-red-700 w-screen">
+      <div className="p-3 bg-red-700 m-0 grid grid-cols-3 grid-rows-1">
         <Link to="/perfil">
           <button type="button">
             <img
               src={ profileIcon }
               alt="profile-icon"
-              className="header-profile-icon"
+              className="bg-red-700"
               data-testid="profile-top-btn"
             />
           </button>
         </Link>
+        <h3
+          className="bg-transparent text-center font-black"
+          data-testid="page-title"
+        >
+          Perfil
+
+        </h3>
       </div>
-      <h3 className="text-center" data-testid="page-title">Perfil</h3>
-      <p data-testid="profile-email">{emailByLocalStorage}</p>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-        onClick={ () => history.push('/receitas-feitas') }
-      >
-        Receitas Feitas
+      <div className="w-screen h-screen flex flex-col justify-center">
+        <p
+          data-testid="profile-email"
+          className="text-center text-xl font-semibold"
+        >
+          {emailByLocalStorage}
+        </p>
+        <section className="text-center gap-10 flex flex-col">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+            onClick={ () => history.push('/receitas-feitas') }
+            className="text-2xl btn btn-outline-danger w-6/12 p-3 self-center "
+          >
+            Receitas Feitas
 
-      </button>
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-        onClick={ () => history.push('/receitas-favoritas') }
-      >
-        Receitas Favoritas
+          </button>
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+            onClick={ () => history.push('/receitas-favoritas') }
+            className="text-2xl btn btn-outline-danger w-6/12 p-3 self-center "
 
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ () => getExit(history) }
-      >
-        Sair
-
-      </button>
+          >
+            Receitas Favoritas
+          </button>
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => getExit(history) }
+            className="text-2xl btn btn-outline-danger w-6/12 p-3 self-center"
+          >
+            LOGOUT
+          </button>
+        </section>
+      </div>
       <Footer />
     </div>
   );
